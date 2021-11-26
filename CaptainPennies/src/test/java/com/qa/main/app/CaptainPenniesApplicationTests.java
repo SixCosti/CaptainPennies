8f66a7255f6d09effbaaba7ee215346b194d5760
Costi <sixcosti@yahoo.com>
2021-11-26 17:00:00 +0000

    started testing

--- CaptainPennies/src/test/java/com/qa/main/app/CaptainPenniesApplicationTests.java
+++ CaptainPennies/src/test/java/com/qa/main/app/CaptainPenniesApplicationTests.java
@@ -1,13 +1,52 @@
 package com.qa.main.app;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Collections;
 
+import javax.swing.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.qa.main.app.actual.service.StudentActualService;
+import com.qa.main.app.model.Student;
+
+@RunWith(Spring.Runner.class)
+@WebMvcTest
 @SpringBootTest
 class CaptainPenniesApplicationTests {
 
+	@Autowired
+	MockMvc mockMvc;
+	
+	@MockBean
+	Student saveStudent;
+	
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception { 
+		
+		Mockito.when(StudentActualService.findAll()).thenReturn(
+				Collections.emptyList()
+				);
+		
+	MvcResult mvcResult =	mockMvc.perform(
+				MockMvcRequestBuilders.get("/getAllStudents")
+				.accept(MediaType.APPLICATION_JSON)
+				).andReturn();
+	System.out.println(mvcResult.getResponse());
+	
+	
+	
+	Mockito.verify(studentActualService).findAll();
+	}
 	}
 
-}
+
