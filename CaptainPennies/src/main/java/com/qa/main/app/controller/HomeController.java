300ba1981a4eb759d0d6e2c802e155d4be17f4fd
Costi <sixcosti@yahoo.com>
2021-11-26 10:10:49 +0000

    added update and delete mapping

--- CaptainPennies/src/main/java/com/qa/main/app/controller/HomeController.java
+++ CaptainPennies/src/main/java/com/qa/main/app/controller/HomeController.java
@@ -1,9 +1,15 @@
 package com.qa.main.app.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.main.app.actual.service.StudentActualService;
@@ -20,9 +26,27 @@ public class HomeController {
 	//To call save method or to save new Student
 	@PostMapping(value = "saveStudent")
 	public Student saveStudent(@RequestBody Student student) {
-		System.out.println("Rock'n'Roll the High School");
+		System.out.println("✨Rock'n'Roll the High School✨");
 		studentActualService.saveStudent(student);
 		return student;
 	}
+	
+	//To find all students
+	@GetMapping(value = "getAllStudents")
+	public List<Student> findAllStudents() {
+		return studentActualService.findAllStudents();
+	}
+	
+	//To update students
+	@PutMapping("updateStudent")
+	public Student updateStudent(@RequestBody Student student) {
+		return studentActualService.updateStudent(student);
+		}	
 		
+	//To delete students
+	@DeleteMapping("deleteStudent")
+	public String deleteStudent(@RequestParam int id) {
+		studentActualService.deleteStudent(id);
+		return "Student Deleted!";
+	}
 }
